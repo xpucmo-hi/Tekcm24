57518d6521481018b5de0432fda884fa2d32ab41
xpucmo-hi <hiroyuki.ishida.hishi@gmail.com>
2024-01-20 07:11:52 +0900

    Add other language option

--- tekcm.py
+++ tekcm.py
@@ -19,7 +19,6 @@ html, body, [class*="css"] {
 """
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-
 @st.cache_resource
 def load_client(api_key):
     return OpenAI(api_key = api_key)
@@ -79,15 +78,18 @@ def playaudio(filename):
 # 定数
 if 'lang_list' not in ss:
     # オプション
-    ss.lang_list = {0: "日本語", 1: "български"}
+    ss.lang_list = {0: "日本語", 1: "Български"}
     ss.lang_code = {0: "ja", 1: "bg"}
     ss.lang_english = {0: "Japanese", 1: "Bulgarian"}
     ss.mode_list = {0: "翻訳", 1: "添削", 2: "会話"}
+    ss.select_show_text = {0: "非表示", 1: "表示"}
     ss.mode_english = {0: "Translate this content into", 1: "Correct the grammer of this content in", 2: "Answer this question within 2 sentences in", 3: "Correct the grammer, and answer this question within 3 sentences in"}
     ss.model_list = ['gpt-3.5-turbo', 'gpt-3.5-turbo-instruct', 'gpt-4']
     ss.select_lang_code = ['be', 'bs', 'bg', 'hr', 'cs', 'hu', 'mk', 'pl', 'ro', 'ru', 'sr', 'sk', 'sl', 'tk']
     ss.select_lang_english = ['Belarusian', 'Bosnian', 'Bulgarian', 'Croatian', 'Czech', 'Hungarian', 'Macedonian', 'Polish', 'Romanian', 'Russian', 'Serbian', 'Slovak', 'Slovenian', 'Turkish', 'Ukrainian']
-    ss.select_lang_list = ['Belarusian', 'Bosnian', 'български', 'Croatian', 'Czech', 'Hungarian', 'Macedonian', 'Polish', 'Romanian', 'Russian', 'Serbian', 'Slovak', 'Slovenian', 'Turkish', 'Ukrainian']
+    ss.select_lang_list = ['Беларуская', 'Bosanski', 'Български', 'Hrvatski', 'Čeština', 'Magyar', 'Македонски', 'Polski', 'Română', 'Русский', 'Српски', 'Slovenčina', 'Slovenščina', 'Türkçe', 'Українська']
+    ss.selected_lang = 2
+    ss.show_text = 1
     ss.lang_selectbox = {i: ss.select_lang_list[i] for i in range(0, len(ss.select_lang_list))}
 
 openai_api_key = os.environ.get("OPENAI_API_KEY")
@@ -95,8 +97,15 @@ with st.sidebar:
     if not openai_api_key:
         openai_api_key = st.text_input("OpenAI API Key", key="api_key", type="password")
     model_select = st.selectbox(label='使用モデル', options=ss.model_list, index=0)
-    lang_index = st.selectbox(label='対象言語', options=(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14), index=2, format_func=lambda x: ss.lang_selectbox.get(x))
+    lang_index = st.selectbox(label='学習言語', options=(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14), index=ss.selected_lang, format_func=lambda x: ss.lang_selectbox.get(x))
+    ss.show_text = st.radio(label='文章表示', options=(0,1), index=1, horizontal=True, format_func=lambda x: ss.select_show_text.get(x))
 
+if ss.selected_lang != lang_index:
+    ss.selected_lang = lang_index
+    ss.lang_list[1] = ss.select_lang_list[lang_index]
+    ss.lang_code[1] = ss.select_lang_code[lang_index]
+    ss.lang_english[1] = ss.select_lang_english[lang_index]
+    
 client = load_client(openai_api_key)
 
 st.title('Tekcm 24 beta')
@@ -121,11 +130,13 @@ if audio_bytes:
         lang_input = 1 - int(lang_output)   # 翻訳モードの場合、入力言語と出力言語は異なる
     with st.spinner('処理中...'):
         text = speech_to_text(audio_bytes, language=ss.lang_code.get(lang_input))
-        st.write(text)
+        if ss.show_text == 1:
+            st.write(text)
 
         # 変換
         textbg = process(task=ss.mode_english.get(mode), lang=ss.lang_english.get(lang_output), content=text, model=str(model_select))
-        st.write(textbg)
+        if ss.show_text == 1:
+            st.write(textbg)
 
         with NamedTemporaryFile(delete=True, suffix=".wav") as temp_file:
             text_to_speech(textbg, temp_file.name)
